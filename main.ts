5886a2b724dddb9512f693aa48114c9fb9b70ffe
cememir <47295445+cememir@users.noreply.github.com>
2021-05-14 10:53:26 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 9 9 9 9 . . . . 
+        . . . . . . 9 9 9 9 5 9 . . . . 
+        . 9 9 9 9 9 9 9 5 5 9 9 . . . . 
+        . . . . . . . 9 9 9 9 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 200, 0)
+})
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    status.spriteAttachedTo().destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -15
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(4, 500)
+})
+let statusbar: StatusBarSprite = null
+let enemyShip: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . 6 4 4 4 
+    . . . . . . . . . . 6 6 6 6 6 . 
+    . . . . . . . . 6 6 6 6 6 . . . 
+    . . . . . . . 6 6 6 6 6 6 . . . 
+    . . . . 6 6 6 6 6 6 6 6 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 a . . . . 
+    6 6 6 6 6 6 6 6 6 6 a a a . . . 
+    . 6 6 6 6 6 6 6 6 6 a a a . . . 
+    . . 8 8 8 8 8 8 8 8 8 a . . . . 
+    . . . . . 6 6 6 6 6 6 6 6 6 . . 
+    . . . . . . . . . . 6 6 6 6 6 . 
+    . . . . . . . . . . . . . 4 4 4 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+info.setLife(5)
+game.onUpdateInterval(2000, function () {
+    enemyShip = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 2 2 . . . . . . 
+        . . . . . . . . 3 2 . . . . . . 
+        . . . . . . . . 3 2 4 . . . . . 
+        . . . . . . . 3 3 2 . . . . . . 
+        . . . . . . . 3 2 2 . . . . . . 
+        . . . . . . 3 2 2 2 . . . . . . 
+        . . . . . 3 3 2 2 2 . . . . . . 
+        . . . 2 3 3 2 2 2 2 . . . . . . 
+        . . 2 2 2 2 2 2 2 2 . . . . . . 
+        . . . . . 3 2 2 2 2 . . . . . . 
+        . . . . . 3 3 2 2 2 . . . . . . 
+        . . . . . . . 3 3 2 4 . . . . . 
+        . . . . . . . . 3 2 . . . . . . 
+        . . . . . . . . . 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    enemyShip.x = scene.screenWidth()
+    enemyShip.vx = -20
+    enemyShip.y = randint(10, scene.screenHeight() - 10)
+    statusbar = statusbars.create(15, 2, StatusBarKind.EnemyHealth)
+    statusbar.setColor(5, 12)
+    statusbar.max = 100
+    statusbar.attachToSprite(enemyShip)
+})
